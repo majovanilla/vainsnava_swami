455e862f34faae1973904f479d9a5c160d1851e6
majovainilla <maria_reyes@me.com>
2020-05-23 21:59:35 -0500

    Add footer component and basic styling

--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import footerClasses from '../styles/footer.module.scss';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import { Typography } from '@material-ui/core';
 
 function Footer() {
   return (
-    <div className="Footer">
-
+    <div className={footerClasses.Footer}>
+      <div className={footerClasses.socialDiv}>
+        <FacebookIcon className={footerClasses.footerIcon}/>
+        <TwitterIcon className={footerClasses.footerIcon}/>
+        <InstagramIcon className={footerClasses.footerIcon}/>
+        <YouTubeIcon className={footerClasses.footerIcon}/>
+      </div>
+      <Typography type="body2">
+        &copy; All rights reserved 2020
+      </Typography>
     </div>
   );
 }
