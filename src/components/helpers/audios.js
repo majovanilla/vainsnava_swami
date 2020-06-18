3cfe42802a27baf0947e9a355fa18e74d938a090
majovainilla <maria_reyes@me.com>
2020-06-18 17:55:42 -0500

    Add podcast card in Audio component

---
+++ src/components/helpers/audios.js
@@ -0,0 +1,40 @@
+const podcasts = [
+  {
+    id: 1,
+    title: 'Live from Argentina',
+    author: 'Swami Vaisnava',
+    img: 'https://images.unsplash.com/photo-1457139621581-298d1801c832?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+  },
+  {
+    id: 2,
+    title: 'Live from India',
+    author: 'Swami Vaisnava',
+    img: 'https://images.unsplash.com/photo-1457139621581-298d1801c832?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+  },
+  {
+    id: 3,
+    title: 'Meditation No.3',
+    author: 'Swami Vaisnava',
+    img: 'https://images.unsplash.com/photo-1457139621581-298d1801c832?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+  },
+  {
+    id: 4,
+    title: 'Meditation No.4',
+    author: 'Swami Vaisnava',
+    img: 'https://images.unsplash.com/photo-1457139621581-298d1801c832?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+  },
+  {
+    id: 5,
+    title: 'Meditation No.5',
+    author: 'Swami Vaisnava',
+    img: 'https://images.unsplash.com/photo-1457139621581-298d1801c832?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+  },
+  {
+    id: 6,
+    title: 'Audio No.6',
+    author: 'Swami Vaisnava',
+    img: 'https://images.unsplash.com/photo-1457139621581-298d1801c832?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+  },
+]
+
+export default podcasts;
--- src/containers/Audios.jsx
+++ src/containers/Audios.jsx
@@ -1,11 +1,52 @@
 import React from 'react';
+import { Container, Typography, Card, CardMedia, CardContent, IconButton } from '@material-ui/core';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import classes from '../styles/audios.module.scss';
+import podcasts from '../components/helpers/audios';
 
 function Audios() {
   return (
-    <div className="Audios">
-
-   
-    </div>
+    <Container className={classes.mainDiv}>
+    <Typography variant="h4" className={classes.mainTitle}>
+      Nuestros Podcasts
+    </Typography>
+    <Typography variant="body1" className={classes.mainText}>
+    </Typography>
+    {podcasts.map((podcast) => {
+      return (
+        <Card className={classes.audioCard}>
+      <div className={classes.details}>
+        <CardContent className={classes.content}>
+          <Typography component="h5" variant="h5">
+            {podcast.title}
+          </Typography>
+          <Typography variant="subtitle1" color="textSecondary">
+            {podcast.author}
+          </Typography>
+        </CardContent>
+        <div className={classes.controls}>
+          <IconButton aria-label="previous">
+            {classes.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
+          </IconButton>
+          <IconButton aria-label="play/pause">
+            <PlayArrowIcon className={classes.playIcon} />
+          </IconButton>
+          <IconButton aria-label="next">
+            {classes.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
+          </IconButton>
+        </div>
+      </div>
+      <CardMedia
+        className={classes.cover}
+        image={podcast.img}
+        title="Live from space album cover"
+      />
+    </Card>
+      )
+    })}
+</Container>
   );
 }
 
