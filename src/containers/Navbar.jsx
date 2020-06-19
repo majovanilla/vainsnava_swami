8bad59346756208646e4b2067915098e99ce82fa
majovainilla <maria_reyes@me.com>
2020-06-19 16:04:25 -0500

    Add functionality to hamburger menu

--- src/containers/Navbar.jsx
+++ src/containers/Navbar.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import HideOnScroll from '../components/Navbar/HideOnScroll';
-import {AppBar, Toolbar, IconButton, Typography, Button} from '@material-ui/core';
+import {AppBar, Toolbar, IconButton, Menu, MenuItem, Button} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import navBarClasses from '../styles/navbar.module.scss';
 
 function Navbar() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClose = event => {
+    setAnchorEl(null);
+  }
+
+  const handleClick = event => {
+    setAnchorEl(event.target);
+  }
+
   return (
     <HideOnScroll>
       <AppBar position="fixed" color="white" className={navBarClasses.navbar}>
         <Toolbar className={navBarClasses.toolBar}>
-          {/* <Typography className={navBarClasses.title}>
-            Vaisnava Swami
-          </Typography> */}
           <div className={navBarClasses.menuDiv}>
             <Link to="/" className={navBarClasses.menuLink}><Button size="large" color="inherit" className={navBarClasses.menuButton}>Inicio</Button></Link>
             <Link to="/historia" className={navBarClasses.menuLink}><Button size="large" color="inherit" className={navBarClasses.menuButton}>Historia</Button></Link>
@@ -21,9 +28,23 @@ function Navbar() {
             <Link to="/contacto" className={navBarClasses.menuLink}><Button size="large" color="inherit" className={navBarClasses.menuButton}>Contacto</Button></Link>
           </div>
     
-          <IconButton edge="start"  color="inherit" aria-label="menu" className={navBarClasses.menuHamburger}>
+          <IconButton edge="start"  color="inherit" aria-label="menu" aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick} className={navBarClasses.menuHamburger}>
             <MenuIcon />
-          </IconButton>
+          </IconButton>    
+          <Menu
+            className={navBarClasses.toggleMenu}
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem><Link to="/" className={navBarClasses.menuLink} onClick={handleClose}><Button size="large" color="inherit" className={navBarClasses.menuButton}>Inicio</Button></Link></MenuItem>
+            <MenuItem><Link to="/historia" className={navBarClasses.menuLink} onClick={handleClose}><Button size="large" color="inherit" className={navBarClasses.menuButton}>Historia</Button></Link></MenuItem>
+            <MenuItem><Link to="/audios" className={navBarClasses.menuLink} onClick={handleClose}><Button size="large" color="inherit" className={navBarClasses.menuButton}>Audios</Button></Link></MenuItem>
+            <MenuItem><Link to="/videos" className={navBarClasses.menuLink} onClick={handleClose}><Button size="large" color="inherit" className={navBarClasses.menuButton}>Videos</Button></Link></MenuItem>
+            <MenuItem><Link to="/contacto" className={navBarClasses.menuLink} onClick={handleClose}><Button size="large" color="inherit" className={navBarClasses.menuButton}>Contacto</Button></Link></MenuItem>
+          </Menu>
         </Toolbar>
       </AppBar> 
     </HideOnScroll>
