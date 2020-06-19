e551292b6acad57b1b81fa7ca8b6f3cd54d9d6d0
majovainilla <maria_reyes@me.com>
2020-06-19 15:37:54 -0500

    Add responsiveness to Audio component

--- src/containers/Audios.jsx
+++ src/containers/Audios.jsx
@@ -9,43 +9,45 @@ import podcasts from '../components/helpers/audios';
 function Audios() {
   return (
     <Container className={classes.mainDiv}>
-    <Typography variant="h4" className={classes.mainTitle}>
-      Nuestros Podcasts
-    </Typography>
-    <Typography variant="body1" className={classes.mainText}>
-    </Typography>
-    {podcasts.map((podcast) => {
-      return (
-        <Card className={classes.audioCard}>
-      <div className={classes.details}>
-        <CardContent className={classes.content}>
-          <Typography component="h5" variant="h5">
-            {podcast.title}
-          </Typography>
-          <Typography variant="subtitle1" color="textSecondary">
-            {podcast.author}
-          </Typography>
-        </CardContent>
-        <div className={classes.controls}>
-          <IconButton aria-label="previous">
-            {classes.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
-          </IconButton>
-          <IconButton aria-label="play/pause">
-            <PlayArrowIcon className={classes.playIcon} />
-          </IconButton>
-          <IconButton aria-label="next">
-            {classes.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
-          </IconButton>
-        </div>
-      </div>
-      <CardMedia
-        className={classes.cover}
-        image={podcast.img}
-        title="Live from space album cover"
-      />
-    </Card>
-      )
-    })}
+      <Typography variant="h4" className={classes.mainTitle}>
+        Nuestros Podcasts
+      </Typography>
+      <Typography variant="body1" className={classes.mainText}>
+      </Typography>
+      <div className={classes.podcastDiv}>
+        {podcasts.map((podcast) => {
+          return (
+            <Card className={classes.audioCard}>
+          <div className={classes.details}>
+            <CardContent className={classes.content}>
+              <Typography component="h5" variant="h5">
+                {podcast.title}
+              </Typography>
+              <Typography variant="subtitle1" color="textSecondary">
+                {podcast.author}
+              </Typography>
+            </CardContent>
+            <div className={classes.controls}>
+              <IconButton aria-label="previous">
+                {classes.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
+              </IconButton>
+              <IconButton aria-label="play/pause">
+                <PlayArrowIcon className={classes.playIcon} />
+              </IconButton>
+              <IconButton aria-label="next">
+                {classes.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
+              </IconButton>
+            </div>
+          </div>
+          <CardMedia
+            className={classes.cover}
+            image={podcast.img}
+            title="Live from space album cover"
+            />
+        </Card>
+          )
+        })}
+    </div>
 </Container>
   );
 }
